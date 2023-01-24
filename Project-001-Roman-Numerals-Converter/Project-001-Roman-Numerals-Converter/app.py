ef93dd9ba7122f451b19319b07ca2cb1cbc5ee82
nwilliams1288 <nicholas.williams1288@gmail.com>
2023-01-23 22:37:35 -0500

    More Stuff5

--- Project-001-Roman-Numerals-Converter/Project-001-Roman-Numerals-Converter/app.py
+++ Project-001-Roman-Numerals-Converter/Project-001-Roman-Numerals-Converter/app.py
@@ -16,14 +16,14 @@ def main_post():
     if request.method == 'POST':
         alpha = str(request.form['number'])        
         if not alpha.isdecimal():
-            return render_template('index.html', developer_name='Altaz', not_valid=True)
+            return render_template('index.html', developer_name='Nick', not_valid=True)
         else:
             number = int(alpha)
             if not 0 < number < 4000:
                 return render_template('index.html', developer_name='Nick', not_valid=True)
             else:
                 roman_number = convert(number)
-                return render_template('result.html', number_decimal=number, number_roman=roman_number, developer_name='Altaz')
+                return render_template('result.html', number_decimal=number, number_roman=roman_number, developer_name='Nick')
     else:
         return render_template('index.html', developer_name='Nick', not_valid=False)
 
